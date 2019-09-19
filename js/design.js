1d1c336c5676f30449fcabc50fcf97cad2eb711f
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2019-09-19 20:13:10 +0100

    add feature to make images on designs page display in a 3 column grid

--- js/design.js
+++ js/design.js
@@ -4,19 +4,24 @@ $(document).ready(function () {
     url: "http://localhost:3000/clothes",
     dataType: "json"
   }).done(function (data) {
-    console.log(data)
+    // alert(data.length)
     for (let i = 0; i < data.length; i++) {
       if (data.length === 0) {
         $(".py-5").html("<h2>There are currently no design collections available</h2>")
       } else {
-        $(".col-10").append(`
-        
+        $("#maryjane").append(`
+        <div class="col-4 mt-5 mb-5">
+        <img class="design image" src="../img/4th.jpg"/>
         <h1 class='text-capitalize font-weight-bold'>${data[i].category}</h1>
         <p class='text-blue'>${data[i].description}</p>
         <p>${data[i].price}</p>
-        <img src="${data[i].imageUrl}"/>
+        </div>
       `)
       }
     }
   })
+
+  function clickSearch(e) {
+
+  }
 })
