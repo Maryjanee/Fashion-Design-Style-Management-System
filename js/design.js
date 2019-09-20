cccbfc468eaecc00572a0b104fffcd4320983ff3
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2019-09-20 05:46:35 +0100

    fixed and edit issued on the webpages

--- js/design.js
+++ js/design.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
       if (data.length === 0) {
         $(".py-5").html("<h2>There are currently no design collections available</h2>")
       } else {
-        $("#maryjane").append(`
+        $("#myRow").append(`
         <div class="col-4 mt-5 mb-5">
         <img class="design image" src="../img/4th.jpg"/>
         <h1 class='text-capitalize font-weight-bold'>${data[i].category}</h1>
@@ -21,7 +21,30 @@ $(document).ready(function () {
     }
   })
 
-  function clickSearch(e) {
 
-  }
+  $('#search-form').submit(function (e) {
+    e.preventDefault();
+    const id = $("#search-field").val();
+    $.ajax({
+      type: "GET",
+      url: `http://localhost:3000/clothes/${id}`,
+      dataType: "json"
+    }).done(function (data) {
+      if (!data) {
+        alert('value does not exit');
+      } else {
+        $("#myRow").append(`
+        <div class="col-4 mt-5 mb-5">
+        <img class="design image" src="../img/4th.jpg"/>
+        <h1 class='text-capitalize font-weight-bold'>${data.category}</h1>
+        <p class='text-blue'>${data.description}</p>
+        <p>${data.price}</p>
+        </div>
+      `)
+      }
+    }
+    )
+  })
+
+
 })
